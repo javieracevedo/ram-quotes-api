c1ab4473ee9ec2a222a45d2fd07197ec5cc9824b
Javier Acevedo <jdacevedog1@gmail.com>
2020-04-28 23:22:15 -0400

    Setup project.

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
-import express from 'express';
-import { json, urlencoded } from 'body-parser';
-import morgan from 'morgan';
-import cors from 'cors';
+import express from 'express'
+import { json, urlencoded } from 'body-parser'
+import morgan from 'morgan'
+import cors from 'cors'
+// TODO: use config files
+const port = 4000
 
-export const app = express();
+export const app = express()
 
-app.disable('x-powered-by');
+app.disable('x-powered-by')
 
 app.use(cors())
 app.use(json())
@@ -13,5 +15,15 @@ app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
 app.get('/random', (req, res ) => {
-  res.status(200).send({ message: "Test" })
+  res.status(200).send({ message: 'Test' })
 })
+
+export const start = async () => {
+  try {
+    app.listen(port, () => {
+      console.log(`REST API on http://localhost:${port}`)
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
