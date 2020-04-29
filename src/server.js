bedeb7a13bf416b23f3a7ca342810855c96b7be8
Javier Acevedo <jdacevedog1@gmail.com>
2020-04-28 22:55:10 -0400

    Initial commit.

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { json, urlencoded } from 'body-parser';
+import morgan from 'morgan';
+import cors from 'cors';
+
+export const app = express();
+
+app.disable('x-powered-by');
+
+app.use(cors())
+app.use(json())
+app.use(urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
+app.get('/random', (req, res ) => {
+  res.status(200).send({ message: "Test" })
+})
